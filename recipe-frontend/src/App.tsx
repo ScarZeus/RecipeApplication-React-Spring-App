21a9cf2564dd6e5e35a18dd0cf6b84d289bb934d
K Kevin Gladson <kevingladson2427@gmail.com>
2025-06-14 09:42:36 +0530

    changes in the router

--- recipe-frontend/src/App.tsx
+++ recipe-frontend/src/App.tsx
@@ -3,14 +3,12 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Home from './components/Home';
 function App() {
   return (
-     <BrowserRouter>
       <Routes>
           <Route path = "/" element = {<Home />}/>
           {/* <Route path = "/login" element = {<Login />}/>
           <Route path = "/register" element = {<Search />}/>
           <Route path = "/user/:id" element = {<UserDetail />}/> */}
       </Routes>
-     </BrowserRouter>
   )
 }
 
